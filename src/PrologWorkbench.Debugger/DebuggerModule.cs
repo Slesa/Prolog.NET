384ea7eaf13b96c20f9795e728f6efa3f6c5bfcd
J. Preiss <joerg.preiss@me.com>
2012-11-02 22:05:00 +0100

    Added debugger module

---
+++ src/PrologWorkbench.Debugger/DebuggerModule.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+
+namespace PrologWorkbench.Debugger
+{
+    public class DebuggerModule : IModule
+    {
+        public void Initialize()
+        {
+            
+        }
+    }
+}
