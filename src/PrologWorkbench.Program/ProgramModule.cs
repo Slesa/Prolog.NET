2fcd5e3713c6842c14722773b2379034a4eee39b
J. Preiss <joerg.preiss@me.com>
2012-11-02 22:17:05 +0100

    Added module Program

---
+++ src/PrologWorkbench.Program/ProgramModule.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+
+namespace PrologWorkbench.Program
+{
+    public class ProgramModule : IModule
+    {
+        public void Initialize()
+        {
+            
+        }
+    }
+}
