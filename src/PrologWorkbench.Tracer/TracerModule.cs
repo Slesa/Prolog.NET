39003dc77b050658f5450cfc5147a80ecddae4a2
J. Preiss <joerg.preiss@me.com>
2012-11-02 22:21:05 +0100

    Added module tracer

---
+++ src/PrologWorkbench.Tracer/TracerModule.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+
+namespace PrologWorkbench.Tracer
+{
+    public class TracerModule : IModule
+    {
+        public void Initialize()
+        {
+            
+        }
+    }
+}
